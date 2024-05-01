204d6a17e0c93e67889a0c20b49a362572ae714b
Andraž Kos <andraz.kos@gmail.com>
2024-05-01 00:08:34 +0000

    Add cors middleware to enable cross-origin resource sharing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import { createServer } from 'node:http'
 import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'node:path'
 import { Server } from 'socket.io'
+import cors from 'cors'
 
 import instructor from './instructor/index.js'
 import createZodSchemaFromJson from './instructor/createZodSchemaFromJson.js'
 
 const app = express()
-const server = createServer(app)
-const io = new Server(server)
+app.use(cors())
 
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+})
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.get('/', (req, res) => {
